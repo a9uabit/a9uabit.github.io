45a164b641a5b62287cc3e58da541ef871e4caf9
a9uabit <63449550+a9uabit@users.noreply.github.com>
2026-09-18 21:46:57 +0900

    styles and icon

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use leptos::{logging, prelude::*};
-use leptos_meta::{Title, provide_meta_context};
+use leptos_meta::{Stylesheet, Title, provide_meta_context};
+use leptos_router::{components::{A, Router}, hooks::use_query_map};
 
 fn main() {
     console_error_panic_hook::set_once();
@@ -17,12 +18,45 @@ fn main() {
 fn App() -> impl IntoView {
     provide_meta_context();
 
+    let query = || use_query_map();
+    let page = move || query().read().get("page").unwrap_or_else(|| "".into());
+
+    view! {
+        <Stylesheet href="/styles.css" />
+
+        <Router>
+            <nav>
+                <A href="/">"Home"</A>
+                <A href="/?page=about">"About"</A>
+            </nav>
+
+            {move || match page().as_str() {
+                "about" => About().into_any(),
+                "" => Home().into_any(),
+                _ => view! { <h1>Not Found</h1> }.into_any(),
+            }}
+        </Router>
+    }
+}
+
+#[component]
+fn Home() -> impl IntoView {
     view! {
         <Title text="Home - a9ua" />
 
-        <div>
-            <h1>"a9ua"</h1>
-            <p>"AKA a9ua_bit, aquaabit"</p>
+        <div class="home">
+            <img src="/icon.png" />
+            <div class="home-name">
+                <h1>"a9ua"</h1>
+                <p>"AKA a9ua_bit, aquaabit"</p>
+            </div>
         </div>
+    }
+}
+
+#[component]
+fn About() -> impl IntoView {
+    view! {
+        <h1>"Hi, I'm a9ua!"</h1>
     }
 }
